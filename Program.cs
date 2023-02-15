22872da8de1b32d178aa57ef5981b2699adb7815
Jhonson03 <ls0303032022@unab.edu.sv>
2023-02-14 19:46:55 -0600

    Creando vista

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using Jhonson18022023.Entidades;
+using Jhonson18022023.Negocio;
+
+Datos datos = new Datos();
+ClsDatos clsDatos = new ClsDatos();
+Console.WriteLine(@"Bienvenido a su contador de caracteres
+A continuación ingrese su palabra o frase, ten en cuenta que los espacios se toman en cuentan");
+
+datos.datos = Console.ReadLine();
+
+Console.WriteLine($"Su frase o palabra contiene: {clsDatos.Contador(datos)} caracteres");
