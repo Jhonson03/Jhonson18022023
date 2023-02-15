6be37f68fa3967755fd90a57458c9d7c91337d76
Jhonson03 <ls0303032022@unab.edu.sv>
2023-02-14 18:48:13 -0600

    Creando Proyecto

---
+++ Negocio/ClsDatos.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jhonson18022023.Negocio
+{
+    public class ClsDatos
+    {
+        
+    }
+}
