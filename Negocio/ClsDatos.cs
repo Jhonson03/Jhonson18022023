5e57b904a52bea9847a5e73e14a597860520d41a
Jhonson03 <ls0303032022@unab.edu.sv>
2023-02-14 18:58:41 -0600

    Creando metodo

--- Negocio/ClsDatos.cs
+++ Negocio/ClsDatos.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Jhonson18022023.Entidades;
 
 namespace Jhonson18022023.Negocio
 {
     public class ClsDatos
     {
-        
+        public int Contador(Datos dat){
+            return dat.datos.Length;
+        }
     }
 }
