2d7e2f4a7500f2cd715eb0cca0bb54a9385fff34
Jhonson03 <ls0303032022@unab.edu.sv>
2023-02-15 09:52:46 -0600

    Vista Actualizada

--- Program.cs
+++ Program.cs
@@ -3,9 +3,17 @@ using Jhonson18022023.Negocio;
 
 Datos datos = new Datos();
 ClsDatos clsDatos = new ClsDatos();
-Console.WriteLine(@"Bienvenido a su contador de caracteres
-A continuación ingrese su palabra o frase, ten en cuenta que los espacios se toman en cuentan");
+Console.WriteLine(@"----------------------------------------
+ Bienvenido a su contador de caracteres
+----------------------------------------
+
+Advertencia: Si la frase que ingresa lleva espacios se contará como un caracter
+
+Presione la tecla 'Enter' para continuar");
+Console.ReadLine();
+
+Console.WriteLine("Ingrese su frase o palabra");
 
 datos.datos = Console.ReadLine();
 
-Console.WriteLine($"Su frase o palabra contiene: {clsDatos.Contador(datos)} caracteres");
+Console.WriteLine($"\nSu frase o palabra contiene: {clsDatos.Contador(datos)} caracteres");
